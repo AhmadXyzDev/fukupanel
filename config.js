ce9b249d8a7d7f2688fa33ba5990ce0b19461953
AhmadXyzDev <reyzaaffreyzafaizramadhan@gmail.com>
2025-11-02 16:20:23 +0800

    update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const config = {
   egg: "15",
   nestid: "5",
   loc: "1",
-  domain: "https://fukushima.brengsek.my.id",
-  apikey: "ptla_4wKuCS4EI99hCWynTPfNyxYYot7Ky56ydJFon8736WO",
-  capikey: "ptlc_FvRahNWtmEjy8P6XXyXw0AvcoNceDDnZSPScOXoxZE4",
+  domain: "https://apip.server.dong.bos.lightsecretaja.web.id",
+  apikey: "ptla_touJHhSTvBcvlR4wEKEw8yCkpmTf4hvC8Ux6GVf8VCe",
+  capikey: "ptlc_77hbi6DYrv3Xz27P7NjvGFMAA8VqBhlHT4qVhwNaVLv",
   storename: "Fukushima Store"
 };
 
